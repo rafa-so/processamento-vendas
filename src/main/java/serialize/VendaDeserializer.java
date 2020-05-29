28653d61cb3c2aad555b01b6c94d4acfcc8d0586
Rafael Oliveira <rafael.oliveira@spot-edu.com.br>
2020-05-28 23:55:53 -0300

    Create processor orders

---
+++ src/main/java/serialize/VendaDeserializer.java
@@ -0,0 +1,26 @@
+package serialize;
+
+import java.io.IOException;
+
+import org.apache.kafka.common.serialization.Deserializer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import model.Venda;
+
+public class VendaDeserializer implements Deserializer<Venda>{
+
+	@Override
+	public Venda deserialize(String topic, byte[] venda) {
+		
+		try {
+			return new ObjectMapper().readValue(venda, Venda.class);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+
+}
